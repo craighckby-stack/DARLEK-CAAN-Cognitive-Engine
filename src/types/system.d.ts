cf49bb992e57a1740fef92f427d0177c28f497a7
Craig Huckerby <craighckby@gmail.com>
2026-08-28 19:57:00 +1000

    EMG Core v49: Neural Optimization on src/types/system.d.ts

--- src/types/system.d.ts
+++ src/types/system.d.ts
@@ -1,48 +1,16 @@
+export type SystemMode = 'development' | 'production' | 'staging';
+
 export interface SystemConfig {
-  version: string;
-  mode: 'development' | 'production';
-  debug: boolean;
+  readonly version: string;
+  readonly mode: SystemMode;
+  readonly debug: boolean;
 }
 
 export interface AgentOrchestratorProps {
-  orchestratorId: string;
-  priority: number;
+  readonly orchestratorId: string;
+  readonly priority: number;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+};
