8cd832ca2fe2f26e00f5f95d2d45523ce363455d
Craig Huckerby <craighckby@gmail.com>
2026-08-28 19:44:21 +1000

    EMG Core v49: Neural Optimization on src/components/ui/separator.tsx

--- src/components/ui/separator.tsx
+++ src/components/ui/separator.tsx
@@ -5,24 +5,32 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
-function Separator({
-  className,
-  orientation = "horizontal",
-  decorative = true,
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive.Root>) {
+export type SeparatorProps = React.ComponentProps<typeof SeparatorPrimitive.Root>
+
+const BASE_STYLES = "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px" as const
+
+export const Separator = React.forwardRef<
+  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  SeparatorProps
+>(function Separator(
+  {
+    className,
+    orientation = "horizontal",
+    decorative = true,
+    ...props
+  },
+  ref
+) {
   return (
     <SeparatorPrimitive.Root
+      ref={ref}
       data-slot="separator"
       decorative={decorative}
       orientation={orientation}
-      className={cn(
-        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-        className
-      )}
+      className={cn(BASE_STYLES, className)}
       {...props}
     />
   )
-}
+})
 
-export { Separator }
+Separator.displayName = SeparatorPrimitive.Root.displayName
