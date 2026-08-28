1a698b56139e8fab6dbdc08bba5fd973e7ec2225
Craig Huckerby <craighckby@gmail.com>
2026-08-28 20:35:37 +1000

    EMG Core v49: Neural Optimization on src/components/ui/aspect-ratio.tsx

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -7,7 +7,7 @@ import { forwardRef } from "react"
 export type AspectRatioProps = ComponentPropsWithoutRef<typeof AspectRatioPrimitive.Root>
 export type AspectRatioElement = ElementRef<typeof AspectRatioPrimitive.Root>
 
-const AspectRatio = forwardRef<AspectRatioElement, AspectRatioProps>(
+export const AspectRatio = forwardRef<AspectRatioElement, AspectRatioProps>(
   function AspectRatio({ ...props }, ref): ReactElement {
     return <AspectRatioPrimitive.Root ref={ref} data-slot="aspect-ratio" {...props} />
   }
